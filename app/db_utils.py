82e0713c27cba1b43511b83f9cba0dbeee78da06
Arseniy <kruchik.a04@gmail.com>
2024-04-30 22:28:18 +0400

    adding app/* dir

---
+++ app/db_utils.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+from sqlalchemy import Engine
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import Session
+
+user = "postgres"
+password = "hackme" # improve secrest managment
+db_name = "postgres"
+db_url = "0.0.0.0"
+sqlalchemy_url = f"postgresql+psycopg2://{user}:{password}"+\
+"@{db_url}/{db_name}"
+
+def create_db_engine(
+    sqlalchemy_url: str
+    ) -> Engine:
+    return create_engine(sqlalchemy_url, echo=True)
+
+def create_session(
+    sqlalchemy_url: str = sqlalchemy_url
+    ) -> Session:
+    return Session(create_db_engine(sqlalchemy_url))
+
---
+++ app/models.py
@@ -0,0 +1,86 @@
+from sqlalchemy import Integer
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import Boolean
+from sqlalchemy import TIMESTAMP
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import MONEY
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "user" 
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(30))
+    second_name: Mapped[str] = mapped_column(String(30))
+    email: Mapped[str] = mapped_column(String(70))
+    phone_number: Mapped[str] = mapped_column(String(13))
+    password: Mapped[str] = mapped_column(String(32)) # for md5 hash algorithm
+    is_super_user: Mapped[bool] = mapped_column(Boolean())
+
+
+class Order(Base):
+    __tablename__ = "order"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    address: Mapped[str] = mapped_column(String(50), nullable=True)
+    date: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True))
+    deliver_date: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True))
+    picup_point_id: Mapped[int] = mapped_column(ForeignKey("picup_point.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+
+class OrderProduct(Base):
+    __tablename__ = "order_product"
+
+    order_id: Mapped[int] = mapped_column(ForeignKey("order.id"), primary_key=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey("product.id"), primary_key=True)
+    cnt: Mapped[int] = mapped_column(Integer())
+
+
+class Product(Base):
+    __tablename__ = "product"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    price: Mapped[MONEY] = mapped_column(MONEY()) # maybe should replace with numeric
+    in_stock: Mapped[int] = mapped_column(Integer())
+    descrption: Mapped[str] = mapped_column(String())
+    photo: Mapped[str] = mapped_column(String())
+    product_type_id: Mapped[int] = mapped_column(ForeignKey("product_type.id"))
+    manufacturer_id: Mapped[int] = mapped_column(ForeignKey("manufacturer.id"))
+
+
+class Manufacturer(Base):
+    __tablename__ = "manufacturer"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+
+
+class ProductType(Base):
+    __tablename__ = "product_type"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+
+
+class PicupPoint(Base):
+    __tablename__ = "picup_point"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    physical_address: Mapped[str] = mapped_column(String(50))
+
+
+class Feedback(Base):
+    __tablename__ = "feedback"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    text: Mapped[str] = mapped_column(String())
+
