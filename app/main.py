fdd489ca60c88339593fe2f8ed739997f87347be
Arseniy <kruchik.a04@gmail.com>
2024-05-02 21:03:46 +0400

    start to developing api, now python app actually work in docker

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("test print means container is up")
+from fastapi import FastAPI
+import uvicorn
+
+from api.main import api_router
+
+
+app = FastAPI(
+    title="myapiv1", version="0.0.1",
+)
+app.include_router(api_router)
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
